2bd59066c4859dc2d72ef815a4f9deebd47e0bf1
Кудрявцев Юрий <phlatline04@gmail.com>
2021-02-14 21:44:20 +0300

    add start options

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,7 +6,7 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation+ " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -14,6 +14,7 @@ testdata = [Group(name="", header="", footer="")] + [
     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
     for i in range(5)
 ]
+
 
 # testdata2 = [
 #     Group(name=name, header=header, footer=footer)
@@ -25,10 +26,12 @@ testdata = [Group(name="", header="", footer="")] + [
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
+    '''
+    :param group: #Bug: If in group parameter <'> symbol.
+    '''
     old_groups = app.group.get_group_list()
     app.group.create(group)
     assert len(old_groups) + 1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
