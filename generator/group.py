13809eeb20c842a7660181d1c6580d7f20f084f4
Кудрявцев Юрий <phlatline04@gmail.com>
2021-02-15 22:09:00 +0300

    reading json

--- generator/group.py
+++ generator/group.py
@@ -1,10 +1,10 @@
-import json
 import os.path
 from model.group import Group
 import random
 import string
 import getopt
 import sys
+import jsonpickle
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
@@ -35,4 +35,5 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_decoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
