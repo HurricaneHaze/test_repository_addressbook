a1e2fdb193160860873ea5b384a9f66c68c2a9bd
Кудрявцев Юрий <phlatline04@gmail.com>
2021-03-02 22:44:05 +0300

    Update db.py

--- fixture/db.py
+++ fixture/db.py
@@ -20,7 +20,7 @@ class DbFixture:
         cursor = self.connection.cursor()
         try:
             cursor.execute(
-                "select group_id, group_name, group_header, group_footer from group_list where deprecated='0000-00-00 00:00:00")
+                "select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
                 group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
@@ -32,7 +32,7 @@ class DbFixture:
         contact_list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select id, firstname, lastname from addressbook")
+            cursor.execute("select id, firstname, lastname from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname) = row
                 contact_list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
