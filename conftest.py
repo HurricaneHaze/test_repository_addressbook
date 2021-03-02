1f7d73598d956b96b2c4a2f462cb4155b0c377a4
Кудрявцев Юрий <phlatline04@gmail.com>
2021-03-02 22:44:11 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -34,14 +34,14 @@ def app(request):
 @pytest.fixture(scope="session", autouse=True)
 def db(request):
     db_config = load_config(request.config.getoption("--target"))['db']
-    dbdixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
+    dbfixture = DbFixture(host=db_config["host"], name=db_config["name"], user=db_config["user"],
                           password=db_config["password"])
 
     def fin():
-        dbdixture.destroy()
+        dbfixture.destroy()
 
     request.addfinalizer(fin)
-    return dbdixture
+    return dbfixture
 
 
 @pytest.fixture(scope="session", autouse=True)
