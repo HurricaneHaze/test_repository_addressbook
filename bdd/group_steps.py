b9446817786980d8b8651b247415ff24d237abfe
Кудрявцев Юрий <phlatline04@gmail.com>
2021-03-02 22:43:54 +0300

    Update group_steps.py

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -1,24 +1,29 @@
 import random
 
+import pytest
 from pytest_bdd import given, when, then
 from model.group import Group
 
 
+@pytest.fixture
 @given('a group list')
 def group_list(db):
     return db.get_group_list()
 
 
+@pytest.fixture
 @given('a group with <name>, <header> and <footer>')
 def new_group(name, header, footer):
     return Group(name=name, header=header, footer=footer)
 
 
+@pytest.fixture
 @when('I add the group to the list')
 def add_new_group(app, new_group):
     app.group.create(new_group)
 
 
+@pytest.fixture
 @then('the new group list is equal to the old list with the added group')
 def verify_group_added(db, group_list, new_group):
     old_groups = group_list
@@ -27,6 +32,7 @@ def verify_group_added(db, group_list, new_group):
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
+@pytest.fixture
 @given('a non-empty group list')
 def non_empty_group_list(db, app):
     if len(db.get_group_list()) == 0:
@@ -34,16 +40,19 @@ def non_empty_group_list(db, app):
     return db.get_group_list()
 
 
+@pytest.fixture
 @given('a random group from the list')
 def random_group(non_empty_group_list):
     return random.choice(non_empty_group_list)
 
 
+@pytest.fixture
 @when('I delete the group from the list')
 def delete_group(app, random_group):
     app.group.delete_group_by_id(random_group.id)
 
 
+@pytest.fixture
 @then('the new group list is equal to the old list with the added group')
 def verify_group_deleted(db, non_empty_group_list, random_group, app, check_ui):
     old_groups = non_empty_group_list
